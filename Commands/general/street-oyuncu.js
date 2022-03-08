d8695482549e1bab0689bf0acef038ebb1dfb19e
Trexhia <54481469+TrexBot66@users.noreply.github.com>
2022-03-08 23:55:18 +0300

    Add files via upload

---
+++ Commands/general/street-oyuncu.js
@@ -0,0 +1,34 @@
+const Config = require('../../Conf/Config.json');
+const Discord = require('discord.js')
+const { mmb, staff } = require('../../Models')
+const moment = require('moment')
+moment.locale('tr')
+module.exports = {
+    name: 'street-oyuncu',
+    enable: true,
+    guildOnly: true,
+    aliases: ['street'],
+    cooldown: {
+        enable: false,
+        onGuild: false,
+        timeOut: 5,
+        msg: 'Bu komutu tekrar kullanabilmek için **{time}** saniye beklemelisin.'
+    },
+    /**
+     * .k @üye isim yaş
+     */
+    run(message, args, client) {
+
+        let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        let serverid = client.guilds.cache.get(Config.Server);
+
+        if (!message.member.permissions.has("ADMINISTRATOR") && !message.member.roles.cache.has(Config.Roles.RegOfficer)) return message.reply('Bu komutu kullanmak için yeterli yetkiye sahip değilsin!')
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!member) return message.reply('Lütfen üyeyi belirtiniz.')
+        let isim = args[1];
+        let yas = args[2];
+        
+        member.roles.add(Config.Roles.streetoyuncu).catch(e => { });
+
+        message.channel.send({ embeds: [new Discord.MessageEmbed().setFooter(message.guild.name).setDescription(`**Kullanıcıya başarılı bir şekilde <@&${Config.Roles.streetoyuncu}> permi verildi.**`)] })
+    }}
---
+++ Commands/general/şöför.js
@@ -0,0 +1,37 @@
+const Config = require('../../Conf/Config.json');
+const Discord = require('discord.js')
+const { mmb, staff } = require('../../Models')
+const moment = require('moment')
+moment.locale('tr')
+module.exports = {
+    name: 'şöför',
+    enable: true,
+    guildOnly: true,
+    aliases: ['şöför', 's', 'ş'],
+    cooldown: {
+        enable: false,
+        onGuild: false,
+        timeOut: 5,
+        msg: 'Bu komutu tekrar kullanabilmek için **{time}** saniye beklemelisin.'
+    },
+    /**
+     * .k @üye isim yaş
+     */
+    run(message, args, client) {
+
+        let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        let serverid = client.guilds.cache.get(Config.Server);
+
+        if (!message.member.permissions.has("ADMINISTRATOR") && !message.member.roles.cache.has(Config.Roles.RegOfficer)) return message.reply('Bu komutu kullanmak için yeterli yetkiye sahip değilsin!')
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!member) return message.reply('Lütfen şöför olacak üyeyi belirtiniz.')
+        let isim = args[1];
+        let yas = args[2];
+        
+        member.roles.add(Config.Roles.şöför).catch(e => { });
+        member.roles.add(Config.Roles.sancarpersonel).catch(e => { });
+        member.roles.add(Config.Roles.sancarplus).catch(e => { });
+        member.roles.remove(Config.Roles.misafir).catch(e => { });
+
+        message.channel.send({ embeds: [new Discord.MessageEmbed().setFooter(message.guild.name).setDescription(`**Kullanıcıya <@&${Config.Roles.şöför}> , <@&${Config.Roles.sancarpersonel}> ve <@&${Config.Roles.sancarplus}> rollerini verdim \nKullanıcıdan <@&${Config.Roles.misafir}> rolünü aldım.**`)] })
+    }}
